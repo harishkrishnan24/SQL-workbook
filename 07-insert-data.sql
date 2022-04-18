8fc8eea013af8972c0e09f91dc835764df4b56e6
Harish Krishnan <h.krishnan@sap.com>
2022-04-18 18:00:32 +0530

    Add conversations table and data

---
+++ 07-insert-data.sql
@@ -0,0 +1,43 @@
+INSERT INTO employers (
+    company_name,
+    company_address,
+    yearly_revenue,
+    is_hiring
+  )
+VALUES (
+    'Learning Inc',
+    'Education Street 12, London',
+    0.87,
+    TRUE
+  )
+INSERT INTO employers (
+    company_name,
+    company_address,
+    yearly_revenue,
+    is_hiring
+  )
+VALUES (
+    'Big Oil Inc',
+    'Slippery Street 110, Houston',
+    112.55,
+    FALSE
+  )
+INSERT INTO employers (
+    company_name,
+    company_address,
+    yearly_revenue,
+    is_hiring
+  )
+VALUES (
+    'Hipster Food',
+    'Avacado Street 5, Berlin',
+    6.12,
+    TRUE
+  )
+INSERT INTO conversations (user_name, employer_name, message, date_sent)
+VALUES (
+    'Max Schwarz',
+    'Learning Inc',
+    'Hi I like learning!',
+    '2021-10-15 16:32:10'
+  )
