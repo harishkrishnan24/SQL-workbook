c4fc0e3f9f9f69ffa6a3c3a38da9925362c6248a
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-04-22 17:13:27 +0530

    Delete tables

---
+++ 10-insert-data-again.sql
@@ -0,0 +1,40 @@
+INSERT INTO employers (
+    company_name,
+    company_address,
+    yearly_revenue,
+    is_hiring
+  )
+VALUES (
+    'Learning Inc',
+    'Education Street 12, London',
+    0.87,
+    TRUE
+  )
+INSERT INTO employers (
+    company_name,
+    company_address,
+    yearly_revenue
+  )
+VALUES (
+    'Big Oil Inc',
+    'Slippery Street 110, Houston',
+    112.55
+  )
+INSERT INTO employers (
+    company_name,
+    company_address,
+    yearly_revenue,
+    is_hiring
+  )
+VALUES (
+    'Hipster Food',
+    'Avacado Street 5, Berlin',
+    6.12,
+    TRUE
+  )
+INSERT INTO conversations (user_name, employer_name, message)
+VALUES (
+    'Max Schwarz',
+    'Learning Inc',
+    'Hi I like learning!'
+  )
